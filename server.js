e622cab3194b2dbf87f05a0c4804ad0844031db8
ISAACLNELSON <isaacschoolacc@gmail.com>
2019-12-27 14:41:54 -0800

    created function that gets form data, does geocode call, and updates data to the database

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const express = require('express');
 const app = express();
 require('ejs');
 require('dotenv').config();
+const methodOverride = require('method-override');
+const superagent = require('superagent');
+
+app.use(methodOverride('_method'));
+
 const PORT = process.env.PORT || 3001;
 const client = require('./lib/client');
 
@@ -17,65 +22,83 @@ const getEventsResults = require('./lib/events/getEventsResults');
 
 // routes
 app.get('/', getHome);
-app.get('/result', getYelpResults);
-app.get('/result', getEventsResults);
+// app.get('/result', getYelpResults);
+// app.get('/result', getEventsResults);
 app.get('/aboutUs', aboutUs);
 app.get('/quiz', displayQuiz);
-app.put('/quiz', submitQuiz);
+app.put('/quiz', getLocPutdb)
 app.delete('/result', deleteDbInfo);
 
 function deleteDbInfo(request, response) {
-  let location = request.location;
-  let sql = 'DELETE FROM user_info WHERE location=$1;';
-  let safeValues = [location];
-  client.query(sql, safeValues);
-  response.redirect('/');
+    let location = request.location;
+    let sql = 'DELETE FROM user_info WHERE location=$1;';
+    let safeValues = [location];
+    client.query(sql, safeValues);
+    response.redirect('/');
 }
 
-if (dbHasContent) {
-  app.put('/quiz', getLocPutdb)
-} else { app.post('quiz', getLocPostdb) }
+
+
 
 function getHome(request, response) {
-  response.render('pages/index');
+    response.render('pages/index');
+    // let sql = 'SELECT * FROM user_info;';
+    // client.query(sql)
+    //     .then(results => {
+    //         response.send(results.rows);
+    //     })
 }
 
 function aboutUs(request, response) {
-  response.render('pages/aboutUs');
+    response.render('pages/aboutUs');
 }
 
-function displayQuiz(response) {
-  response.render('pages/quiz');
+function displayQuiz(request, response) {
+    response.render('pages/quiz');
 }
 
-function getLocPostdb(request, response) {
-// invoke the geocode function, assign it to a variable
-
-let { hunger, interest, music} = request.body;
-let sql = 'INSERT INTO user_info (hunger, interest, music) VALUES ($1, $2, $3);';
-let safeValues =  [hunger, interest, music];
-client.query(sql, safeValues);
-response.redirect('/result'); 
+let getLocation = function(city){
+    // let city = request.body.location;
+    let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${process.env.GEOCODE_API_KEY}`
+    superagent.get(url)
+    .then(results => {
+        let locationObject = {
+            location: results.body.results[0].formatted_address,
+            lat: results.body.results[0].geometry.location.lat,
+            lng: results.body.results[0].geometry.location.lng
+        }
+        return locationObject;
+    })
 }
 
 function getLocPutdb(request, response) {
-  // invoke the geocode function, assign it to a variable 
-  let { hunger, interest, music} = request.body;
-  let sql = 'UPDATE user_info hunger=$1, interest=$2, music=$3 WHERE user_id=1;';
-  let safeValues =  [hunger, interest, music];
-  client.query(sql, safeValues);
-  response.redirect('/result'); 
-  }
+    let city = request.body.location;
+    let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${process.env.GEOCODE_API_KEY}`
+    superagent.get(url)
+    .then(results => {
+        let locationObject = {
+            location: results.body.results[0].formatted_address,
+            lat: results.body.results[0].geometry.location.lat,
+            lng: results.body.results[0].geometry.location.lng
+        }
+        // let city = getLocation(request.body.location)
+        
+        let { hunger, interest, music } = request.body;
+        // console.log(hunger, interest, music);
+        let sql = 'UPDATE user_info SET hunger=$1, interest=$2, music=$3, location=$5, lat=$6, long=$7 WHERE user_id=$4 returning user_id;';
+        let safeValues = [hunger, interest, music, 1, locationObject.location, locationObject.lat, locationObject.lng];
+        client.query(sql, safeValues);
+        response.redirect('/result');
 
-
-
-function submitQuiz(request, response) {
-
+    })
 }
 
 
-app.use('*', (request, response) => {
-  response.status(404).send('page not found');
-});
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+app.use('*', (request, response) => {
+    response.status(404).send('page not found');
+});
+client.connect(() => {
+    app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+})
