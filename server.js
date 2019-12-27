1412affb3c28b0e0ea9f8df2555c71bb3276e35f
laura antonius <laurastolen@gmail.com>
2019-12-27 10:45:49 -0800

    made flag to indicate whether DB has info

--- server.js
+++ server.js
@@ -5,9 +5,26 @@ const app = express();
 require('ejs');
 require('dotenv').config();
 const PORT = process.env.PORT || 3001;
+const client = require('./lib/client');
+
+// determine whether DB has any content, which determines the value of flag
+let dbHasContent;
+let sql = 'SELECT * FROM user_info;';
+client.query(sql)
+  .then(results => {
+    if (results.rowCount) {
+      dbHasContent = true;
+      // need to declare getLocPutDB function which makes the geocode api call, stores info in db and redirects to results
+      app.put('/quiz', getLocPutDB)
+    }
+    else {
+      dbHasContent = false;
+      app.post('/quiz', getLocPostDB)
+    }
+  });
 
 app.set('view engine', 'ejs');
-app.use(express.static('/public'));
+app.use(express.static('./public'));
 app.use(express.urlencoded());
 
 // api JS
@@ -17,7 +34,7 @@ const getEventsResults = require('./lib/events/getEventsResults');
 
 // routes
 app.get('/', getHome);
-app.post('/result', getYelpResults);
+app.get('/result', getYelpResults);
 app.get('/result', getEventsResults);
 app.get('/aboutUs', aboutUs);
 app.get('/quiz', displayQuiz);
