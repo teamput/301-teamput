a1313faead5b1c4b72405fb3fb4a0bcec275db48
Vik Akam <vikramakam@hotmail.com>
2019-12-31 10:46:05 -0800

    modularized save to favorites func

--- public/app.js
+++ public/app.js
@@ -2,23 +2,19 @@
 
 //on menu click open or close nav bar for mobile
 $(function mobileClick() {
-    $('header').hide();
-    $('#hamburger').on('click', () => {
-        //show or hide header with transition
-        console.log('clicked');
-        $('#header').show();
-        $('header').slideToggle();
-    });
+  $('header').hide();
+  $('#hamburger').on('click', () => {
+    //show or hide header with transition
+    console.log('clicked');
+    $('#header').show();
+    $('header').slideToggle();
+  });
 })
 mobileClick();
 
-$(main).click(
-    alert('bla')
-)
-
 $.ajax({
-    url: '/result',
-    method: 'POST',
-    data: { data: resturaunt }
-}).then 
+  type: 'POST',
+  dataType: 'json',
+}).then(results => {
+  console.log(results.body);
 })
