3303b375cc5dc6eaa4b69afe2c2b3e35004082d9
Isaac <isaacschoolacc@gmail.com>
2020-01-01 17:28:31 -0800

    added see more options to music, events, and news

--- public/app.js
+++ public/app.js
@@ -10,7 +10,7 @@ $(function mobileClick() {
     $('#menu').slideToggle();
   });
 })
-mobileClick();
+// mobileClick();
 
 $.ajax({
   type: 'POST',
@@ -40,3 +40,63 @@ function radioButtons() {
 }
 radioButtons();
 
+function foodOptions(){
+  
+  let currentFood = 1;
+  $('#food-section section').toggle();
+  $(`#food-section section:nth-of-type(${currentFood})`).toggle();
+  
+
+  $('.seeNextFood').on('click', () => {
+    currentFood += 1;
+    $(`#food-section section:nth-of-type(${currentFood})`).slideDown();
+  })
+  
+}
+foodOptions();
+
+function musicOptions(){
+  
+  let currentmusic = 1;
+  $('#music-section section').toggle();
+  $(`#music-section section:nth-of-type(${currentmusic})`).toggle();
+  
+
+  $('.seeNextMusic').on('click', () => {
+    currentmusic += 1;
+    $(`#music-section section:nth-of-type(${currentmusic})`).slideDown();
+  })
+  
+}
+musicOptions();
+
+
+function eventOptions(){
+  
+  let currentevent = 1;
+  $('#event-section section').toggle();
+  $(`#event-section section:nth-of-type(${currentevent})`).toggle();
+  
+
+  $('.seeNextEvent').on('click', () => {
+    currentevent += 1;
+    $(`#event-section section:nth-of-type(${currentevent})`).slideDown();
+  })
+  
+}
+eventOptions();
+
+function newsOptions(){
+  
+  let currentnews = 1;
+  $('#news-section section').toggle();
+  $(`#news-section section:nth-of-type(${currentnews})`).toggle();
+  
+
+  $('.seeNextNews').on('click', () => {
+    currentnews += 1;
+    $(`#news-section section:nth-of-type(${currentnews})`).slideDown();
+  })
+  
+}
+newsOptions();
